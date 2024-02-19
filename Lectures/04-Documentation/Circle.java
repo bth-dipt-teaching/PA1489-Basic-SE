a290b0baf4bcd74d768239404184bd0325b969b7
mickesv <Mikael.Svahnberg@gmail.com>
2024-02-19 14:18:19 +0100

    Lecture: Documentation

---
+++ Lectures/04-Documentation/Circle.java
@@ -0,0 +1,24 @@
+/**
+ * Represents a Circle that is defined by a framing rectangle.
+ * 
+ * This class acts as a superclass of all shapes vaguely round.
+ * 
+ * @author Mikael Svahnberg
+ * @version 1.0
+ */
+public class Circle {
+
+/**
+ * Sets the location of the Shape
+ * 
+ * @param xPosition the X coordinate of the upper-left corner of the framing rectangle.
+ * @param yPosition the Y coordinate of the upper-left corner of the framing rectangle.
+ * 
+ * @return true if it was possible to move the Shape.
+ * 
+ * @see java.awt.geom.Ellipse2D
+ */
+public boolean move(int xPosition, int yPosition) {
+}
+
+}
